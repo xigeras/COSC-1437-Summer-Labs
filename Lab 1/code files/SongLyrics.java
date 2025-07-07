e76896306b63e7b8b15812794275ed7cbce14d26
august b. <69170271+xigeras@users.noreply.github.com>
2025-07-07 23:56:18 +0000

    SongLyrics created

---
+++ Lab 1/code files/SongLyrics.java
@@ -0,0 +1,9 @@
+public class SongLyrics {
+    public static void main(String[] args)
+    {
+        System.out.println("One way, or another, I'm gonna find ya");
+        System.out.println("I'm gonna meet ya, meet ya, meet ya, meet ya");
+        System.out.println("One way, or another, I'm gonna win ya, I'll get ya, I'll get ya");
+        System.out.println("One way, or another, I'm gonna see ya");
+    }
+}
