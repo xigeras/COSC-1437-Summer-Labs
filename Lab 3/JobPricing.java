dc9f19d963c5430f351402a90b7f918b0acf1ffc
august b. <69170271+xigeras@users.noreply.github.com>
2025-07-10 01:03:41 +0000

    finished job pricing

--- Lab 3/JobPricing.java
+++ Lab 3/JobPricing.java
@@ -6,6 +6,31 @@
  * Pass the numeric data to a method that computes an estimate for the job and returns the 
  * computed value to the main() method, where the job name and estimated price are displayed. 
  * Save the program as JobPricing.java.
- */
+*/
+import java.util.Scanner;
 
- 
+public class JobPricing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("What is the name of the job?");
+        String jobName = scanner.nextLine();
+        System.out.println("What is the cost of materials?");
+        int costOfMaterials = scanner.nextInt();
+        System.out.println("Number of hours of work required?");
+        int hoursWorkRequired = scanner.nextInt();
+        System.out.println("Number of hours of travel time?");
+        int hoursTravelTime = scanner.nextInt();
+
+        int estimate = estimatedCost(costOfMaterials, hoursWorkRequired, hoursTravelTime);
+
+        System.out.println("Job Name: " + jobName + "\nEstimated Cost: $" + estimate);
+        scanner.close();
+    }
+
+    public static int estimatedCost(int costOfMaterials, int hoursWorkRequired, int hoursTravelTime) 
+    {
+        int estimatedEverything = (costOfMaterials + (35 * hoursWorkRequired) + (12 * hoursTravelTime));
+        return estimatedEverything;
+    }
+}
