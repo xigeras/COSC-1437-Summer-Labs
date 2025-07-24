79ecdf9a579cc3a46006ea868535607c681d12e6
august b. <69170271+xigeras@users.noreply.github.com>
2025-07-24 17:45:07 +0000

    done deal

--- Lab 6/Factorials.java
+++ Lab 6/Factorials.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * Write an application that displays the factorial for every integer value from 1 to a user-entered limit. 
  * A factorial of a number is the product of that number multiplied by each positive integer lower than it. 
@@ -7,4 +9,28 @@
  * Save the file as Factorials.java.
  */
 
- 
+ public class Factorials {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer limit (1 to 16 recommended): ");
+        int limit = scanner.nextInt();
+
+        if (limit < 1) {
+            System.out.println("Please enter a number greater than 0.");
+        } else {
+            System.out.println("Factorials from 1 to " + limit + ":");
+            for (int i = 1; i <= limit; i++) {
+                int factorial = 1;
+                for (int j = 1; j <= i; j++) {
+                    factorial += j;
+                }
+                System.out.println(i + "! = " + factorial);
+            }
+
+            if (limit > 16) {
+                System.out.println("Note that Factorials above 16 may be incorrect due to integer overflow.");
+            }
+        }
+        scanner.close();
+    }
+ }
